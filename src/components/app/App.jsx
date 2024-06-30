e0a6eca22b00180ca42a67a79b31d88aee82bf3e
Web-Mastermind <AZEAgent@gmail.com>
2024-06-30 23:35:53 +0400

    form component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import AppFilter from "../app-filter/App-filter"
 import AppInfo from "../app-info/App-info"
+import EmployesAddForm from "../employes-add-form/employes-add-form"
 import EmployesList from "../employes-list/employes-list"
 import SearchPanel from "../search-panel/Search-panel"
 import "./app.css"
@@ -13,6 +14,7 @@ const App = () => {
         <AppFilter />
       </div>
       <EmployesList />
+      <EmployesAddForm />
     </div>
   )
 }
